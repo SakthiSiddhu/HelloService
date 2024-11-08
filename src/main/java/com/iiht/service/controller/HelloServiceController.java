920740f68f2d917afcd5df810d3375ccf10a426a
D V Bharath Kamal <123236582+DatlaBharath@users.noreply.github.com>
2024-11-08 16:04:09 +0530

    Update HelloServiceController.java

--- src/main/java/com/iiht/service/controller/HelloServiceController.java
+++ src/main/java/com/iiht/service/controller/HelloServiceController.java
@@ -42,7 +42,7 @@ public class HelloServiceController {
 	           "</head>" +
 	           "<body>" +
 	           "<div class='container'>" +
-	           "<h1>Congratulations! Application is Deployed</h1>" +
+	           "<h1>Congratulations! Application is Deployed. Test Run</h1>" +
 	           "<p>Your application is up and running successfully!</p>" +
 	           "</div>" +
 	           "</body>" +
